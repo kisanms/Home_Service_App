a7d0e016d9cdcfb0bdc06cb125bfb6d99af7d833
Kisan Majumdar <kisanmajumdar44@gmail.com>
2024-09-28 22:09:49 +0530

    user will be logged in by secure store

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import * as SecureStore from "expo-secure-store";
 import SplashScreen from "./SplashScreenView";
 import Navigate from "./navigation/navigate";
-import { LogBox } from "react-native";
-import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { LogBox, Text, View } from "react-native";
+import {
+  ClerkProvider,
+  ClerkLoaded,
+  SignedIn,
+  SignedOut,
+} from "@clerk/clerk-expo";
 import { Slot } from "expo-router";
-
+const tokenCache = {
+  async getToken(key) {
+    try {
+      const item = await SecureStore.getItemAsync(key);
+      if (item) {
+        console.log(`${key} was used 🔐 \n`);
+      } else {
+        console.log("No values stored under key: " + key);
+      }
+      return item;
+    } catch (error) {
+      console.error("SecureStore get item error: ", error);
+      await SecureStore.deleteItemAsync(key);
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 if (!publishableKey) {
@@ -21,17 +50,27 @@ export default function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsShowSplash(false);
-    }, 3000);
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
 
   LogBox.ignoreLogs(["Linking requires a build-time setting scheme"]);
+  LogBox.ignoreLogs(["Clerk: Clerk has been loaded with development keys"]);
 
   return (
-    <ClerkProvider publishableKey={publishableKey}>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
       <ClerkLoaded>
-        {isShowSplash ? <SplashScreen /> : <Navigate />}
+        <SignedIn>
+          <Text style={{ paddingTop: 20 }}>You are signed in</Text>
+        </SignedIn>
+        {isShowSplash ? (
+          <SplashScreen />
+        ) : (
+          <SignedOut>
+            <Navigate />
+          </SignedOut>
+        )}
       </ClerkLoaded>
     </ClerkProvider>
   );
