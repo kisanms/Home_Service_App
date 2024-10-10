ea7e6ab2bcf578fd56ab01f377512cf00f7111c7
Kisan Majumdar <kisanmajumdar44@gmail.com>
2024-10-10 22:33:05 +0530

    booking modal with new component pageheading

--- screens/BusinessDetailsScreen/BookingModal.jsx
+++ screens/BusinessDetailsScreen/BookingModal.jsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import PageHeading from '../../app/Components/PageHeading '
 
 export default function BookingModal() {
   return (
     <View>
-      <Text>BookingModal</Text>
+      <PageHeading title={'Booking'} />
     </View>
   )
 }
--- screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
+++ screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
@@ -1,9 +1,10 @@
 import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
-import Ionicons from '@expo/vector-icons/Ionicons';
+
 import GlobalApi from '../../utils/GlobalApi';
 import BusinessListItem from './BusinessListItem';
+import PageHeading from '../../app/Components/PageHeading ';
 
 export default function BusinessListByCategoryScreen() {
   const navigation = useNavigation();
@@ -20,11 +21,7 @@ export default function BusinessListByCategoryScreen() {
   }
   return (
     <View style={{ padding: 20, paddingTop: 40 }}>
-      <TouchableOpacity style={{ display: 'flex', flexDirection: 'row', gap: 10, alignItems: "center" }}
-        onPress={() => navigation.goBack()}>
-        <Ionicons name="arrow-back-outline" size={30} color="black" />
-        <Text style={{ fontFamily: "outfit-medium", fontSize: 25 }}>{param.category}</Text>
-      </TouchableOpacity>
+      <PageHeading title={param.category} />
       {businessList?.length > 0 ? <FlatList
         style={{ marginTop: 10 }}
         data={businessList}
