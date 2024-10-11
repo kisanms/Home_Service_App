fda9649e1f7440b050f0af9776ac44aeae90d4f8
Kisan Majumdar <kisanmajumdar44@gmail.com>
2024-10-11 22:42:46 +0530

    calender added

--- screens/BusinessDetailsScreen/BookingModal.jsx
+++ screens/BusinessDetailsScreen/BookingModal.jsx
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import Ionicons from '@expo/vector-icons/Ionicons';
+import CalendarPicker from "react-native-calendar-picker";
+import Color from '../../utils/Color';
 
 export default function BookingModal({ hideModal }) {
   return (
@@ -10,8 +12,21 @@ export default function BookingModal({ hideModal }) {
         <Ionicons name="arrow-back-outline" size={30} color="black" />
         <Text style={{ fontFamily: "outfit-medium", fontSize: 25 }}>Booking</Text>
       </TouchableOpacity>
+      {/*Calender section */}
+      <View style={styles.calenderContainer}>
+        <CalendarPicker onDateChange={this.onDateChange}
+          width={340}
+          minDate={Date.now()}
+          todayBackgroundColor={Color.PRIMARY} />
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  calenderContainer: {
+    backgroundColor: Color.PRIMARY_LIGHT,
+    padding: 20,
+    borderRadius: 15,
+  }
+})
